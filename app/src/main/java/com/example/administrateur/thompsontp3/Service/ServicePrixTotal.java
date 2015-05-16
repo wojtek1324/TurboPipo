a7ea4d4f3075c31e66f5d369e6f4e306de41d300
wojtek <j.wojtek.thompson@gmail.com>
2015-05-16 13:30:25 -0400

    MAJ service produit total

--- app/src/main/java/com/example/administrateur/thompsontp3/Service/ServicePrixTotal.java
+++ app/src/main/java/com/example/administrateur/thompsontp3/Service/ServicePrixTotal.java
@@ -1,12 +1,25 @@
 package com.example.administrateur.thompsontp3.Service;
 
+import com.example.administrateur.thompsontp3.Model.AchatItem;
 import com.example.administrateur.thompsontp3.Model.RabaisCourant;
 import com.example.administrateur.thompsontp3.Model.Transaction;
+import com.example.administrateur.thompsontp3.Model.TransactionItem;
+
+import java.util.List;
 
 /**
  * Created by 1263287 on 2015-05-12.
  */
 public class ServicePrixTotal {
+
+    private List<TransactionItem> listPourCalcul;
+    private RabaisCourant rabaisCourant;
+
+    public ServicePrixTotal(Transaction pTransaction, RabaisCourant pRabaisCourant)
+    {
+        this.listPourCalcul = pTransaction.Transactions;
+        this.rabaisCourant = pRabaisCourant;
+    }
 
     public static Transaction AppliquerRabaisEtTaxes(Transaction pTransaction, RabaisCourant pRabaisCourant)
     {
@@ -16,11 +29,14 @@ public class ServicePrixTotal {
         return pTransaction;
     }
 
-    public double calculer2Pour1(double pPrixCourant ,Transaction pTransaction, RabaisCourant pRabaisCourant)
+    public void calculer2Pour1(Transaction pTransaction, RabaisCourant pRabaisCourant)
     {
-        double prix2Pour1 = 0;
 
-        return prix2Pour1;
+        for(AchatItem itemAchat : pRabaisCourant.getList2Pour1())
+        {
+
+        }
+
     }
 
     public boolean prixAuDelaDuSeuilExemptionTaxes(double prix, RabaisCourant pRabaisCourant)
@@ -41,6 +57,15 @@ public class ServicePrixTotal {
     public void AjouterProduitsGratuits(Transaction pTransaction, RabaisCourant pRabaisCourant)
     {
 
+    }
+
+    private boolean transactionContient(AchatItem item)
+    {
+        boolean contientAchatItem = false;
+        
+
+
+        return contientAchatItem;
     }
 
 
